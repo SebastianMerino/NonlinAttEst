3d5d39daa4de05e882dc8173bcbff0efadd2748b
SebastianMerino <109495963+SebastianMerino@users.noreply.github.com>
2024-09-23 15:42:55 -0500

    bf changes

--- beamforming.m
+++ beamforming.m
@@ -1,5 +1,7 @@
-baseDir = 'C:\Users\smerino.C084288\Documents\MATLAB\BA_AC_joint\rfdata';
-fileSam = 'rf_prebf_BaAttRef.mat';
+setup,
+
+baseDir = 'C:\Users\smerino.C084288\Documents\Datasets\Nonlinearity';
+fileSam = 'rf_prebf_BaAttIncFreq5.mat';
 load(fullfile(baseDir,fileSam))
 
 focalNumber = 2;
@@ -11,7 +13,7 @@ offset = round(offset/100/dz)+1;
 rf1 = BFangle(rf1,length(x),fs,1540, pitch,'bh',focalNumber,0);
 rf2 = BFangle(rf2,length(x),fs,1540, pitch,'bh',focalNumber,0);
 
-save(fullfile(baseDir,'rf_ba8_att0p12_ref.mat'),...
+save(fullfile(baseDir,'rf_BaAttIncFreq5.mat'),...
     'rf1','rf2','x','z','fs');
 
 rf1 = rf1(offset:end,:);
@@ -20,18 +22,19 @@ bmode1 = db(hilbert(rf1));
 bmode1 = bmode1 - max(bmode1(:));
 
 figure,
-imagesc(x*100,z*100,bmode1, [-40 0])
+imagesc(x*100,z*100,bmode1, [-50 0])
 colormap gray
+rectangle('Position',[-0.8,2.5-0.8,1.6,1.6], 'Curvature',1, ...
+    'EdgeColor','w', 'LineStyle','--', 'LineWidth',1)
 axis image
 %%
-baseDir = 'C:\Users\smerino.C084288\Documents\MATLAB\BA_AC_joint\rfdata';
-fileSam = 'rf_prebf_BaAttInc1.mat';
+fileSam = 'rf_prebf_BaAttIncFreq6.mat';
 load(fullfile(baseDir,fileSam))
 
 rf1 = BFangle(rf1,length(x),fs,1540, pitch,'bh',focalNumber,0);
 rf2 = BFangle(rf2,length(x),fs,1540, pitch,'bh',focalNumber,0);
 
-save(fullfile(baseDir,'rf_baBack6_baInc9_att0p1.mat'),...
+save(fullfile(baseDir,'rf_BaAttIncFreq6.mat'),...
     'rf1','rf2','x','z','fs');
 
 rf1 = rf1(offset:end,:);
@@ -40,18 +43,19 @@ bmode1 = db(hilbert(rf1));
 bmode1 = bmode1 - max(bmode1(:));
 
 figure,
-imagesc(x*100,z*100,bmode1, [-40 0])
+imagesc(x*100,z*100,bmode1, [-50 0])
 colormap gray
+rectangle('Position',[-0.8,2.5-0.8,1.6,1.6], 'Curvature',1, ...
+    'EdgeColor','w', 'LineStyle','--', 'LineWidth',1)
 axis image
 %%
-baseDir = 'C:\Users\smerino.C084288\Documents\MATLAB\BA_AC_joint\rfdata';
-fileSam = 'rf_prebf_BaAttInc2.mat';
+fileSam = 'rf_prebf_BaAttIncFreq7.mat';
 load(fullfile(baseDir,fileSam))
 
 rf1 = BFangle(rf1,length(x),fs,1540, pitch,'bh',focalNumber,0);
 rf2 = BFangle(rf2,length(x),fs,1540, pitch,'bh',focalNumber,0);
 
-save(fullfile(baseDir,'rf_ba9_attBack0p1_attInc0p18.mat'),...
+save(fullfile(baseDir,'rf_BaAttIncFreq7.mat'),...
     'rf1','rf2','x','z','fs');
 
 rf1 = rf1(offset:end,:);
@@ -60,27 +64,27 @@ bmode1 = db(hilbert(rf1));
 bmode1 = bmode1 - max(bmode1(:));
 
 figure,
-imagesc(x*100,z*100,bmode1, [-40 0])
+imagesc(x*100,z*100,bmode1, [-50 0])
 colormap gray
+rectangle('Position',[-0.8,2.5-0.8,1.6,1.6], 'Curvature',1, ...
+    'EdgeColor','w', 'LineStyle','--', 'LineWidth',1)
 axis image
-
 %%
-baseDir = 'C:\Users\smerino.C084288\Documents\MATLAB\BA_AC_joint\rfdata';
-fileSam = 'rf_prebf_BaAttInc3.mat';
-load(fullfile(baseDir,fileSam))
-
-rf1 = BFangle(rf1,length(x),fs,1540, pitch,'bh',focalNumber,0);
-rf2 = BFangle(rf2,length(x),fs,1540, pitch,'bh',focalNumber,0);
-
-save(fullfile(baseDir,'rf_baBack6_baInc9_attBack0p1_attInc0p18.mat'),...
-    'rf1','rf2','x','z','fs');
-
-rf1 = rf1(offset:end,:);
-z = z(offset:end);
-bmode1 = db(hilbert(rf1));
-bmode1 = bmode1 - max(bmode1(:));
-
-figure,
-imagesc(x*100,z*100,bmode1, [-40 0])
-colormap gray
-axis image
+% fileSam = 'rf_prebf_BaAttInc3.mat';
+% load(fullfile(baseDir,fileSam))
+% 
+% rf1 = BFangle(rf1,length(x),fs,1540, pitch,'bh',focalNumber,0);
+% rf2 = BFangle(rf2,length(x),fs,1540, pitch,'bh',focalNumber,0);
+% 
+% save(fullfile(baseDir,'rf_baBack6_baInc9_attBack0p1_attInc0p18.mat'),...
+%     'rf1','rf2','x','z','fs');
+% 
+% rf1 = rf1(offset:end,:);
+% z = z(offset:end);
+% bmode1 = db(hilbert(rf1));
+% bmode1 = bmode1 - max(bmode1(:));
+% 
+% figure,
+% imagesc(x*100,z*100,bmode1, [-40 0])
+% colormap gray
+% axis image
